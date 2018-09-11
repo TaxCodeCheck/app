14ba4cdefa7d513a52950c08ba1234fc59d34ea8
Suzanne Richman <suzanne@favoredfortune.com>
2018-09-11 09:26:51 -0700

    commenting out settings on search per discussion with Tara

--- app/src/main/java/com/example/taxcodecheck/LoginActivity.java
+++ app/src/main/java/com/example/taxcodecheck/LoginActivity.java
@@ -64,10 +64,10 @@ public class LoginActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
